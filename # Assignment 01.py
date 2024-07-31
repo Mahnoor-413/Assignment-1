e9792c460dbc7f2d0a4f4bbe1c28cd6e4eee0724
Mahnoor-413 <mahnoornaseem97@gmail.com>
2024-07-31 20:13:54 +0500

    Add files via upload

---
+++ # Assignment 01.py
@@ -0,0 +1,12 @@
+# Assignment 1
+print('''Myself Essay
+     Hello! My name is Mahnoor.
+      I am from Faisalabad.
+      I am  17 years old.
+      I am currently passout ICS.
+      I enjoy to recite the Holy Quran daily.
+      In the future, I hope to develop something new in the field of AI.
+      I am very passionate about this field.
+      Iam new in this field.
+      Thank you for reading my essay!'''
+       ) 
